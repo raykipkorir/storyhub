dc1e381cb18ada7e2161712fc966a24ddd3297d3
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-04-07 13:47:23 +0300

    configure docker

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -e
+set -x
+
+cd storyhub
+python3 manage.py makemigrations --no-input
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
+
+gunicorn storyhub.storyhub.wsgi
