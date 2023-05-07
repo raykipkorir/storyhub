149e6788ccfaeaa3c44fb6731886aa6e4295cec4
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-05-07 09:44:02 +0300

    feat: add post view count

--- storyhub/posts/admin.py
+++ storyhub/posts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import BookmarkPost, LikePost, Post
+from .models import BookmarkPost, LikePost, Post, PostViews
 
 
 admin.site.register(Post)
 admin.site.register(BookmarkPost)
 admin.site.register(LikePost)
+admin.site.register(PostViews)
